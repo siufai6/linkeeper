4fad934381c2e52bddbbe690c273cc2d9bba0967
T14\siufa <siufai6@gmail.com>
2024-07-03 11:11:00 +0100

    added add boomark by just sending a link. refactor code

--- data.py
+++ data.py
@@ -1,61 +1,78 @@
 # Database setup
 import sqlite3
+from linkeeper import logger
 
 DB_NAME = 'bookmarks.db'
-
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS bookmarks
-                 (id INTEGER PRIMARY KEY, url TEXT, title TEXT, description TEXT, tags TEXT)''')
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS bookmarks
+                     (id INTEGER PRIMARY KEY, url TEXT, title TEXT, description TEXT, tags TEXT)''')
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+    except sqlite3.Error as e:
+        logger.error(f"Failed to initialize database: {e}")
+    finally:
+        conn.close()
 
 def search_by_url_pattern(url):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    query="SELECT * FROM bookmarks WHERE url LIKE ?"
-    c.execute(query,(url,))
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        query = "SELECT * FROM bookmarks WHERE url LIKE ?"
+        c.execute(query, (url,))
 
-    bookmarks = c.fetchall()
-    conn.close()
-    return bookmarks
+        bookmarks = c.fetchall()
+        return bookmarks
+    except sqlite3.Error as e:
+        logger.error(f"Failed to search bookmarks by URL pattern: {e}")
+        return []
 
 def search_by_exact_url(url):
-    conn = sqlite3.connect(DB_NAME)
-    print(f'{url}xxxxxxxx')
-    c = conn.cursor()
-    query="SELECT * FROM bookmarks WHERE url=?"
-    c.execute(query,(url,))
-    bookmarks = c.fetchall()
-    conn.close()
-    print(bookmarks)
-    return bookmarks
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        query = "SELECT * FROM bookmarks WHERE url=?"
+        c.execute(query, (url,))
+        bookmarks = c.fetchall()
+        return bookmarks
+    except sqlite3.Error as e:
+        logger.error(f"Failed to search bookmarks by exact URL: {e}")
+        return []
+
+def delete_bookmark_by_id(id):
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute('DELETE FROM bookmarks WHERE id= ?', (id,))
+        conn.commit()
+    except sqlite3.Error as e:
+        logger.error(f"Failed to delete bookmark: {e}")
+    finally:
+        conn.close()
+
+def list_bookmarks():
+    try:
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute("SELECT * FROM bookmarks")
+        bookmarks = c.fetchall()
+        return bookmarks
+    except sqlite3.Error as e:
+        logger.error(f"Failed to retrieve bookmarks: {e}")
+        return []
 
 # Save bookmark
 def save_bookmark(url, title=None, description=None, tags=None):
     conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute('''INSERT INTO bookmarks (url, title, description, tags)
-                    VALUES (?, ?, ?, ?)''', (url, title, description, tags))
-    conn.commit()
-    conn.close()
+    try :
+        c = conn.cursor()
+        c.execute('''INSERT INTO bookmarks (url, title, description, tags)
+                        VALUES (?, ?, ?, ?)''', (url, title, description, tags))
+        conn.commit()
+    except sqlite3.Error as e:
+        logger.error(f"Failed to save bookmark: {e}")
+    finally:
+        conn.close()
 
-
-# Save bookmark
-def delete_bookmark_by_id(id):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute('delete from bookmarks where id= ?', (id,))
-    conn.commit()
-    conn.close()
-
-# List bookmarks
-def list_bookmarks():
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("SELECT * FROM bookmarks")
-    bookmarks = c.fetchall()
-    conn.close()
-    return bookmarks
--- linkeeper.py
+++ linkeeper.py
@@ -13,55 +13,64 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+TAG_DELIMITER="|"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     welcome_message = (
         "Welcome to Linkeeper! 📚✨\n\n"
         "I'm here to help you store and organize your bookmarks. "
         "Here are some basic instructions:\n\n"
-        "• Use /add [URL] to save a new bookmark\n"
+        "• Use /add [URL] to save a new bookmark or just paste a link to add\n"
         "• Use /ls to see all your saved bookmarks\n"        
         "• Use /se [tag] [keyword] to search for bookmark\n\n"
         "• Use /rm [n]  to delete bookmark with id n \n\n"
+        "• You can also reply to a message with /rm to delete the bookmark\n\n"
         "Happy bookmarking!"
     )
     await update.message.reply_text(welcome_message)
 
 
+def extract_url_and_tags(input_string):
+    # Pattern to match URLs with or without protocol
+    url_pattern = r'(https?:\/\/)?([www\.]?[^\s]+\.[a-z]{2,})(\/[^\s#?]*)?(\?[^#\s]*)?(?=#|\s|$)'
+    
+    # Pattern to match hashtags
+    tag_pattern = r'#(\w+)'
+    
+    # Extract URL
+    url_match = re.search(url_pattern, input_string)
+    url = url_match.group().rstrip('/') if url_match else None
+    
+    # Extract tags
+    tags = re.findall(tag_pattern, input_string)
+    
+    return (url, tags)
 
+
+def get_tags_for_save(tags_arr, delimiter=TAG_DELIMITER):
+    return delimiter.join(tags_arr) if len(tags_arr)>0 else ''
 
 async def add_bookmark(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # add code to add bookmark to database
+    """
+    this function will handle the /add command to add a bookmark
+    """
     args = context.args
-    if len(args) == 0:
+    text = ' '.join(args)
+    url, tags_arr = extract_url_and_tags(text)
+    tags=get_tags_for_save(tags_arr)
+    if url is None:
         await update.message.reply_text('No URL provided. e.g to add google.com, type /add google.com.')
-        return
-        
-    url = args[0]
-    tags_arr = re.findall(r'#(\w+)', ' '.join(args[1:]))  # This regex finds all words preceded by a '#'
-    tags = "|".join(tags_arr)  
     try:
-        bkm = data.search_by_exact_url(url)
-        if len(bkm) > 0:
-            logger.info(f'Bookmark already exists. {bkm[0][0]} {bkm[0][1]} {bkm[0][4]}')
-            # remove old bookmarks
-            data.delete_bookmark_by_id(bkm[0][0])
-            await update.message.reply_text('The Bookmark already exists, it has been updated.')
-        else:
-            logger.info(f"Adding bookmark with url {url} and tags {tags}")
-        # Adjust the save_bookmark call according to how many tags you found
-        if len(tags) == 0:
-            data.save_bookmark(url=url)
-        else:
-            data.save_bookmark(url=url, title=None, description=None, tags=tags)
-
-        await update.message.reply_text('Bookmark added!')
+        await process_bookmark(update, url, tags)
     except Exception as e:
         logger.error(f"Failed to add bookmark: {e}")
         await update.message.reply_text('Sorry, there was an error adding your bookmark.')
 
 
 async def rm_bookmark(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+    this function will handle the /rm command to remove a bookmark
+    """
     # add code to add bookmark to database
     args = context.args
     if len(args) == 0:
@@ -72,13 +81,16 @@ async def rm_bookmark(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logger.info(f"Removing bookmark with id {id}")
     try:
         data.delete_bookmark_by_id(id=id)
-
         await update.message.reply_text('Bookmark removed!')
     except Exception as e:
         logger.error(f"Failed to remove bookmark: {e}")
         await update.message.reply_text('Sorry, there was an error removing your bookmark.')
 
+
 async def handle_reply_command(update: Update, context):
+    """
+    this function will handle the command when it is a reply to a message, e.g. when the user sends /rm in reply to a message
+    """
     command = update.message.text  # This will be the command
     original_message = update.message.reply_to_message
     logger.info(f"Received command: {command}")
@@ -101,6 +113,18 @@ async def handle_reply_command(update: Update, context):
         await rm_bookmark(update, context)
 
 
+#def get_url(text):
+#    match = re.search(r'^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$', text) is not None
+#    if match:
+#        return match.group(0)  # Return the matched URL
+#    else:
+#        return None
+
+def format_tags(tag_string, delimiter=TAG_DELIMITER):
+    if tag_string is None:
+        return ''
+    tags_arr = tag_string.split(delimiter)
+    return ' '.join([f'#{tag}' for tag in tags_arr])
 
 async def search_bookmark(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
@@ -117,10 +141,8 @@ async def search_bookmark(update: Update, context: ContextTypes.DEFAULT_TYPE):
         for bookmark in bookmarks:
             i+=1
             tags=''
-            print(bookmark)
             if bookmark[4] is not None:
-                tags_arr = bookmark[4].split('|')
-                tags = ' '.join([f'#{tag}' for tag in tags_arr])
+                tags = format_tags(bookmark[4])
             await update.message.reply_text(f'ID:{bookmark[0]}: {bookmark[1]} {tags}')
     else:
         await update.message.reply_text('No matching bookmarks.')
@@ -135,24 +157,49 @@ async def list_bookmarks(update: Update, context: ContextTypes.DEFAULT_TYPE):
         for bookmark in bookmarks:
             i+=1
             tags=''
-            print(bookmark)
             if bookmark[4] is not None:
-                tags_arr = bookmark[4].split('|')
-                tags = ' '.join([f'#{tag}' for tag in tags_arr])
+                tags = format_tags(bookmark[4])
+                #tags_arr = bookmark[4].split('|')
+                #tags = ' '.join([f'#{tag}' for tag in tags_arr])
             await update.message.reply_text(f'ID:{bookmark[0]}: {bookmark[1]} {tags}')
     else:
         await update.message.reply_text('You have no bookmarks saved.')
 
 
 async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+        this function will handle the text when it is not a command, e.g. when the user sends a URL
+    """
     # You can access the text of the message with [`update.message.text`](command:_github.copilot.openSymbolFromReferences?%5B%7B%22%24mid%22%3A1%2C%22path%22%3A%22%2Fc%3A%2Fworking%2Flinkeeper%2Flinkeeper.py%22%2C%22scheme%22%3A%22file%22%7D%2C%7B%22line%22%3A30%2C%22character%22%3A23%7D%5D "../linkeeper/linkeeper.py")
     text = update.message.text
-    if is_url(text):
-        await update.message.reply_text(f"URL detected: {text}")
-        add_bookmark(update, context)
-    else:
-        await update.message.reply_text(f"Not an URL. Nothing to do")
+    url, tags_arr = extract_url_and_tags(text)
+    logger.info(f"{url} {tags_arr}")
 
+    tags=get_tags_for_save(tags_arr)
+    if url is not None:
+        await process_bookmark(update, url, tags)
+
+
+
+async def process_bookmark(update, url, tags):
+    message = f"URL detected: {url}"
+    existing_bookmark = data.search_by_exact_url(url)
+    if len(existing_bookmark) > 0:
+        bookmark_id, _, _, _, existing_tags = existing_bookmark[0]
+        logger.info(f'Bookmark already exists. {bookmark_id} {url} {existing_tags}')
+        # remove old bookmarks
+        data.delete_bookmark_by_id(bookmark_id)
+        message="Bookmark already exists and  updated."
+    else:
+        message="Bookmark added."
+        logger.info(f"Adding bookmark with url {url} and tags {tags}")
+        # Adjust the save_bookmark call according to how many tags you found
+    if len(tags) == 0:
+        data.save_bookmark(url=url)
+    else:
+        data.save_bookmark(url=url, title=None, description=None, tags=tags)
+
+    await update.message.reply_text(message)
 
 
 
